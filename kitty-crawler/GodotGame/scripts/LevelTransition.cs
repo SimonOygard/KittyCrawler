4880cc1476a584e8c24d7d9ad691e673500e1753
Lou <196540309+Louflu@users.noreply.github.com>
2026-03-27 08:38:30 +0100

    Test/Louise into DEV for base project (#7)
    * La inn assets i mapper
    
    * la til Tileset assets
    
    * Testmap
    
    Lite testmap med collision og wasd funksjonalitet.
    
    * Utvidet map, lagt til directional animation, lagt til camera
    
    (WIP) Utvidet map, mye igjen å gjøre her.
    Lagt til directional animation.
    Lagt til camera med PhantomCamera plugin som gjør at kamera følger spriten
    
    * La til litt collision
    
    * Redid parts of the map
    
    * z indeks endring
    
    * added enemy and enemy scene transition
    
    * La til Fade transition
    
    Still WIP, må fikse collision igjen og fadeout
    
    * added fadeout
    
    * la til plugin og rydda litt i filstrukturen
    
    * started adding dialog

---
+++ kitty-crawler/GodotGame/scripts/LevelTransition.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System;
+
+public partial class LevelTransition : Area2D
+{
+    [Export] public string ScenePath { get; set; } = string.Empty;
+    [Export] public string EnemyName { get; set; } = "Skeleton";
+
+    private bool _triggered = false;
+
+    private async void _on_body_entered(Node body)
+    {
+        GD.Print("Collision detected with: " + body.Name);
+
+        if (_triggered) return;
+
+        if (body.IsInGroup("player"))
+        {
+            GD.Print("Player detected!");
+            _triggered = true;
+
+            GD.Print("About to load scene: " + ScenePath);
+
+            var battleScene = GD.Load<PackedScene>(ScenePath);
+            GD.Print("Scene loaded");
+
+            var instance = battleScene.Instantiate();
+            GD.Print("Scene instantiated");
+
+            if (instance is BattleScene battle)
+            {
+                battle.EnemyName = EnemyName;
+                battle.ReturnScenePath = GetTree().CurrentScene.SceneFilePath;
+                GD.Print("Enemy name assigned");
+            }
+            GD.Print("About to switch scenes");
+
+            await FadeTransition.Instance.FadeToBlack();
+
+            GetTree().Root.AddChild(instance);
+            GetTree().CurrentScene.QueueFree();
+            GetTree().CurrentScene = instance;
+
+            await FadeTransition.Instance.FadeFromBlack();
+
+            GD.Print("Scene transition finished");
+        }
+    }
+}
