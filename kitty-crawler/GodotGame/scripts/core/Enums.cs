2a1bf3cda3b5dd8eae42c93cebd6157d2f5ea363
Simon Øygard <Simon.oygard@hotmail.com>
2026-03-31 11:08:47 +0200

    Movement er i boks
    Movement er i boks.
    
    Floor 2 er i gang.

--- kitty-crawler/GodotGame/scripts/core/Enums.cs
+++ kitty-crawler/GodotGame/scripts/core/Enums.cs
@@ -1,13 +1,25 @@
-using Godot;
-using System;
+namespace Game.Core;
 
-namespace Game.Core
+public enum LogLevel
 {
-    public enum LogLevel
-    {
-        DEBUG,
-        INFO,
-        WARNING,
-        ERROR,
-    }
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+}
+
+public enum ECharacterAnimation
+{
+    idle_down,
+    idle_up,
+    idle_left,
+    idle_right,
+    turn_down,
+    turn_up,
+    turn_left,
+    turn_right,
+    walk_down,
+    walk_up,
+    walk_left,
+    walk_right
 }
