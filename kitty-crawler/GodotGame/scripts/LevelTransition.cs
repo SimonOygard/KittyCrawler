0dfebcd36cbf2c4396f4514f32a7b69ff4567ea5
Louflu <196540309+Louflu@users.noreply.github.com>
2026-04-10 16:31:46 +0200

    Modifisert LevelTransition for utvidet bruk, fjernet ubrukt scene
    Endret på leveltransition.cs slik at man kan sette klasser i godot på triggers. Lagt til diverse enums for puzzle, traps osv.
    
    Ikke i bruk: enemytransition.tscn så slettet

--- kitty-crawler/GodotGame/scripts/LevelTransition.cs
+++ kitty-crawler/GodotGame/scripts/LevelTransition.cs
@@ -4,6 +4,10 @@ using System;
 public partial class LevelTransition : Area2D
 {
     [Export] public string ScenePath { get; set; } = string.Empty;
+
+    [Export] public TransitionType Type { get; set; } = TransitionType.Battle;
+
+    // Battlespecific transition
     [Export] public string EnemyName { get; set; } = "Skeleton";
 
     private bool _triggered = false;
@@ -19,21 +23,13 @@ public partial class LevelTransition : Area2D
             GD.Print("Player detected!");
             _triggered = true;
 
-            GD.Print("About to load scene: " + ScenePath);
-
-            var battleScene = GD.Load<PackedScene>(ScenePath);
+            var newScene = GD.Load<PackedScene>(ScenePath);
             GD.Print("Scene loaded");
 
-            var instance = battleScene.Instantiate();
+            var instance = newScene.Instantiate();
             GD.Print("Scene instantiated");
 
-            if (instance is BattleScene battle)
-            {
-                battle.EnemyName = EnemyName;
-                battle.ReturnScenePath = GetTree().CurrentScene.SceneFilePath;
-                GD.Print("Enemy name assigned");
-            }
-            GD.Print("About to switch scenes");
+            SetScene(instance);
 
             await FadeTransition.Instance.FadeToBlack();
 
@@ -46,4 +42,54 @@ public partial class LevelTransition : Area2D
             GD.Print("Scene transition finished");
         }
     }
+
+    private void SetScene(Node instance)
+    {
+        GD.Print("About to load scene: " + ScenePath);
+        switch (Type)
+        {
+            case TransitionType.Battle:
+                if (instance is BattleScene battle)
+                {
+                    battle.EnemyName = EnemyName;
+                    battle.ReturnScenePath = GetTree().CurrentScene.SceneFilePath;
+                    GD.Print("Enemy name assigned");
+                }
+                else
+                {
+                    GD.Print("Transition type is Battle but scene is not a BattleScene");
+                }
+                break;
+
+            case TransitionType.Teleport:
+                // Teleport-specific logic can be added here
+                break;
+            case TransitionType.Puzzle:
+                // Puzzle-specific logic can be added here
+                break;
+            case TransitionType.Trap:
+                // Trap-specific logic can be added here
+                break;
+            case TransitionType.Dialogue:
+                // Dialogue-specific logic can be added here
+                break;
+            case TransitionType.Door:
+                // Door-specific logic can be added here
+                break;
+            case TransitionType.Generic:
+                // Generic transition logic can be added here
+                break;
+        }
+    }
+}
+
+public enum TransitionType
+{
+    Battle,
+    Teleport,
+    Puzzle,
+    Trap,
+    Dialogue,
+    Door,
+    Generic
 }
