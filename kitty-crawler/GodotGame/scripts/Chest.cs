6dc6188e5e18b2dbe9cdecc67421e179835f3fa8
Louflu <196540309+Louflu@users.noreply.github.com>
2026-04-22 15:39:02 +0200

    Teleport, redid Spade map, mimic

--- kitty-crawler/GodotGame/scripts/Chest.cs
+++ kitty-crawler/GodotGame/scripts/Chest.cs
@@ -2,15 +2,17 @@ using Godot;
 using Interaction;
 using System;
 
+namespace ChestInteractions;
+
 public partial class Chest : CharacterBody2D, IInteractable
 {
     private AnimatedSprite2D _sprite;
     private bool _hasBeenInteractedWith = false;
-    private bool _isMimic = false;
+    public bool IsMimic { get; set; } = false;
 
     [Export]
     private LevelTransition _levelTransition;
-
+    
     public override void _Ready()
     {
         _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
@@ -21,9 +23,6 @@ public partial class Chest : CharacterBody2D, IInteractable
         {
             _sprite.Play("idle");
         }
-
-        _isMimic = true; // new Random().Next(1, 11) == 10; // 10% chance to be a mimic
-        GD.Print("Chest " + Name + " is a mimic: " + _isMimic);
 
     }
 
@@ -36,7 +35,7 @@ public partial class Chest : CharacterBody2D, IInteractable
 
         _hasBeenInteractedWith = true;
 
-        if (_isMimic)
+        if (IsMimic)
         {
             TriggerMimic();
             GD.Print("Mimic has been interacted with " + Name);
