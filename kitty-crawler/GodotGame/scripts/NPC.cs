7aecb69bf6971c785687c86592c657c9bb8c00f7
Simon Øygard <Simon.oygard@hotmail.com>
2026-05-04 18:39:03 +0200

    NPCer og Dialog
    Laget NPCer og dialog til de.
    Du kan velge å battle med de eller ikke.

--- kitty-crawler/GodotGame/scripts/NPC.cs
+++ kitty-crawler/GodotGame/scripts/NPC.cs
@@ -1,61 +1,78 @@
 using Godot;
 using Interaction;
-using System;
-
+using DialogueManagerRuntime;
 
 public partial class NPC : CharacterBody2D, IInteractable
 {
     private AnimatedSprite2D _sprite;
     private bool _hasBeenInteractedWith = false;
+    private bool _wantsBattle = false;
 
     [Export] private LevelTransition _levelTransition;
     [Export] private KittyCrawler.TELT.NPCData _npcData;
+    [Export] private Resource _dialogueResource;
 
     public override void _Ready()
     {
         _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-        _levelTransition =  GetNode<LevelTransition>("LevelTransition");
+        _levelTransition = GetNode<LevelTransition>("LevelTransition");
 
         if (_sprite != null)
-        {
             _sprite.Play("skester_idle");
-        }
 
+        DialogueManager.DialogueEnded += OnDialogueEnded;
     }
 
     public void Interact()
     {
-        if (_hasBeenInteractedWith)
+        if (_hasBeenInteractedWith) return;
+
+        if (_dialogueResource != null)
         {
-            return;
+            var states = new Godot.Collections.Array<Variant>();
+            states.Add(Variant.From(this));
+            DialogueManager.ShowDialogueBalloon(_dialogueResource, "start", states);
         }
+        else
+        {
+            _hasBeenInteractedWith = true;
+            TriggerCardbattle();
+        }
+    }
 
-        _hasBeenInteractedWith = true;
+    // Kalles av do start_battle() i dialogen
+    public void StartBattle()
+    {
+        GD.Print("[NPC] StartBattle kalt!");
+        _wantsBattle = true;
+    }
 
-        TriggerCardbattle();
-        GD.Print("NPC has been interacted with " + Name);
+    private void OnDialogueEnded(Resource resource)
+    {
+        GD.Print($"[NPC] DialogueEnded: resource={resource?.ResourcePath}, dialogueResource={_dialogueResource?.ResourcePath}, wantsBattle={_wantsBattle}");
 
+        if (resource != _dialogueResource) return;
+
+        if (_wantsBattle)
+        {
+            _hasBeenInteractedWith = true;
+            TriggerCardbattle();
+        }
     }
 
     private void TriggerCardbattle()
     {
-        GD.Print($"TriggerCardbattle kalt, npcData={_npcData?.NPCName ?? "null"}");
-
         if (_npcData != null)
         {
             TeltBattleConfig.Instance.CurrentNPC = _npcData;
-            GD.Print("Telt battle triggered for NPC: " + GetTree().CurrentScene.SceneFilePath);
             TeltBattleConfig.Instance.ReturnScenePath = GetTree().CurrentScene.SceneFilePath;
         }
         else
             GD.PrintErr("NPCData ikke satt på NPC: " + Name);
 
-        GD.Print($"LevelTransition er null: {_levelTransition == null}");
-        GD.Print($"ScenePath: {_levelTransition?.ScenePath}");
-
         if (_levelTransition != null)
             _levelTransition.TriggerTransition();
         else
-            GD.PrintErr("LevelTransition node is not assigned for NPC: " + Name);
+            GD.PrintErr("LevelTransition ikke assignet for NPC: " + Name);
     }
 }
