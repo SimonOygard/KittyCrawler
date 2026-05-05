340974bc6aac774aef401269764ded459335faac
Simon Øygard <164393107+SimonOygard@users.noreply.github.com>
2026-05-05 22:34:35 +0200

    Update Boss.cs
    Tilbakestilt Boss.cs til Dev sin versjon

--- kitty-crawler/GodotGame/scripts/Boss.cs
+++ kitty-crawler/GodotGame/scripts/Boss.cs
@@ -1,69 +1,46 @@
 using Godot;
 using Interaction;
-using DialogueManagerRuntime;
-using KittyCrawler.TELT;
+using System;
+
 
 public partial class Boss : CharacterBody2D, IInteractable
 {
     private AnimatedSprite2D _sprite;
+    private bool _hasBeenInteractedWith = false;
 
     [Export] private LevelTransition _levelTransition;
-    [Export] private BossData _bossData;
-    [Export] private Resource _dialogueResource;
+    [Export] private KittyCrawler.TELT.BossData _bossData;
 
     public override void _Ready()
     {
         _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-        _levelTransition = GetNode<LevelTransition>("LevelTransition");
+        _levelTransition =  GetNode<LevelTransition>("LevelTransition");
 
         if (_sprite != null && _sprite.Name == "Skester")
-            _sprite.Play("skester_idle");
-        else
-            _sprite.Play("default");
-
-        if (_bossData != null
-            && PlayerData.HasDefeatedNpc(_bossData.NpcId)
-            && TeltBattleConfig.Instance.JustReturnedFromBattle)
         {
-            TeltBattleConfig.Instance.JustReturnedFromBattle = false;
-            CallDeferred(nameof(StartPostBattleDialogue));
+            _sprite.Play("skester_idle");
         }
-    }
-
-    private void StartPostBattleDialogue()
-    {
-        var states = new Godot.Collections.Array<Variant>();
-        states.Add(Variant.From(this));
-        DialogueManager.ShowDialogueBalloon(_dialogueResource, "PostBattle", states);
+        else
+        {
+            _sprite.Play("default"); 
+        }
     }
 
     public void Interact()
     {
-        if (_bossData == null) return;
-
-        bool hasDefeated = PlayerData.HasDefeatedNpc(_bossData.NpcId);
-
-        if (hasDefeated)
+        if (_hasBeenInteractedWith)
         {
-            var states = new Godot.Collections.Array<Variant>();
-            states.Add(Variant.From(this));
-            DialogueManager.ShowDialogueBalloon(_dialogueResource, "PostBattle", states);
             return;
         }
 
-        if (_dialogueResource != null)
-        {
-            var states = new Godot.Collections.Array<Variant>();
-            states.Add(Variant.From(this));
-            DialogueManager.ShowDialogueBalloon(_dialogueResource, "start", states);
-        }
-        else
-        {
-            TriggerBossCardbattle(); // ← rettet navn
-        }
+        _hasBeenInteractedWith = true;
+
+        TriggerCardbattle();
+        GD.Print("Boss has been interacted with " + Name);
+
     }
 
-    private void TriggerBossCardbattle()
+    private void TriggerCardbattle()
     {
         if (_bossData != null)
         {
@@ -77,6 +54,6 @@ public partial class Boss : CharacterBody2D, IInteractable
         if (_levelTransition != null)
             _levelTransition.TriggerTransition();
         else
-            GD.PrintErr("LevelTransition ikke assignet for Boss: " + Name);
+            GD.PrintErr("LevelTransition node is not assigned for Boss: " + Name);
     }
 }
